99c7635579888bbb4fa2d29af19e445944f5119f
Jenda1981 <jenaprindaa@gmail.com>
2025-11-13 22:21:16 +0100

    updt2

--- aplikace do skoly/src/app/api/lessons/route.ts
+++ aplikace do skoly/src/app/api/lessons/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { getPublishedLessons } from "@/server/store";
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   const lessons = await getPublishedLessons();
